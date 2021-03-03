5e9fb78eab7adb7bd12a8c942da9ae7582ce6d21
Sagorika28 <2sagorika8@gmail.com>
2021-03-03 15:18:14 +0530

    naive multiplicative modulo inverse

---
+++ Week1/Mathematics/Modulo_inverse.cpp
@@ -0,0 +1,39 @@
+// O(m)
+#include <iostream>
+using namespace std;
+
+class Solution
+{
+public:
+    //Complete this function
+    int modInverse(int a, int m)
+    {
+        for (int i = 1; i < m; i++)
+        {
+            if (((a % m) * (i % m)) % m == 1)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
+
+int main()
+{
+    int T;
+
+    //taking testcases
+    cin >> T;
+    while (T--)
+    {
+        int a, m;
+
+        //taking input a and m
+        cin >> a >> m;
+        Solution ob;
+        //calling function modInverse()
+        cout << ob.modInverse(a, m) << endl;
+    }
+    return 0;
+}
